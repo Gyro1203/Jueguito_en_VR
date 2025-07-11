f063198c7a8aa77ef279e1b4c0b62e9f91784816
Gyro1203 <sebastian.vasquez2101@alumnos.ubiobio.cl>
2025-07-10 22:20:17 -0400

    Menu de pausa (Faltan detalles)

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("UI Pages")]
+    public GameObject menu;
+    public GameObject restartConfirm;
+    public GameObject mainMenuConfirm;
+
+    [Header("Pause Menu Buttons")]
+    public Button resumeButtom;
+    public Button restartButton;
+    public Button confirmRestartButton;
+    public Button mainMenuButton;
+    public Button confirmMainMenuButton;
+
+    public List<Button> returnButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnableMenu();
+
+        //Hook events
+        resumeButtom.onClick.AddListener(ResumeGame);
+        restartButton.onClick.AddListener(EnableRestartConfirm);
+        confirmRestartButton.onClick.AddListener(RestartGame);
+        mainMenuButton.onClick.AddListener(EnableMainMenuConfirm);
+        confirmMainMenuButton.onClick.AddListener(ReturnToMainMenu);
+
+        foreach (var item in returnButtons)
+        {
+            item.onClick.AddListener(EnableMenu);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void ReturnToMainMenu()
+    {
+        HideMenu();
+        SceneTransitionManager.singleton.GoToSceneAsync(0);
+    }
+
+    public void HideMenu()
+    {
+        menu.SetActive(false);
+        restartConfirm.SetActive(false);
+        mainMenuConfirm.SetActive(false);
+    }
+
+    public void EnableMenu()
+    {
+        menu.SetActive(true);
+        restartConfirm.SetActive(false);
+        mainMenuConfirm.SetActive(false);
+    }
+
+    public void EnableRestartConfirm()
+    {
+        menu.SetActive(false);
+        restartConfirm.SetActive(true);
+        mainMenuConfirm.SetActive(false);
+    }
+    public void EnableMainMenuConfirm()
+    {
+        menu.SetActive(false);
+        restartConfirm.SetActive(false);
+        mainMenuConfirm.SetActive(true);
+    }
+}
