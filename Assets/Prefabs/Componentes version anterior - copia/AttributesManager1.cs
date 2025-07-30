2d13fee01808015db7c1010afd56e142d634e509
Gyro1203 <130713326+Gyro1203@users.noreply.github.com>
2025-07-30 09:57:09 -0400

    TEST

---
+++ Assets/Prefabs/Componentes version anterior - copia/AttributesManager1.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AttributesManager1 : MonoBehaviour
+{
+    public float attack;
+    public float maxHealth;
+    [HideInInspector]
+    public float currentHealth;
+    public bool imPlayer, imDying;
+    public FadeScreen fadeScreen;
+    public DiedScreen diedScreen;
+
+    [SerializeField] public int currentLevel, currentExperience, maxExperience, expValue;
+
+    private GameObject player;
+
+    private SpawnManagerScript spawnManager;
+ 
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(gameObject.CompareTag("Player")){
+            imPlayer = true;
+            imDying = false;
+        }
+        currentHealth = maxHealth;
+        spawnManager = FindObjectOfType<SpawnManagerScript>();
+    }
+
+    void Update()
+    {
+        if(currentHealth <= 0)
+        { 
+
+            if(spawnManager != null)
+            {
+                spawnManager.enemiesKilled();
+            }
+
+            if(!imPlayer)
+            {
+                Debug.Log("VALOR DE EXP AL MATAR UN ENEMIGO: " + expValue);
+                ExperienceManager.Instance.AddExperienceHandler(player, expValue);
+                //Destroy(gameObject);
+            }
+            else if(!imDying)
+            {
+                imDying = true;
+                fadeScreen.fadeDuration = 10;
+                diedScreen.gameObject.SetActive(true);
+                diedScreen.FadeIn();
+                player.GetComponent<ActionBasedContinuousMoveProvider>().enabled = false;
+                var interactors = player.GetComponentsInChildren<XRBaseInteractor>();
+                foreach (var interactor in interactors)
+                {
+                    interactor.enabled = false;
+                }
+                
+                SceneTransitionManager.singleton.GoToSceneAsync(0);
+            }
+        }
+    }
+
+    public void DealDamage(GameObject target)
+    {
+        var atm = target.GetComponent<AttributesManager1>();
+        if(atm != null)
+        {
+            atm.TakeDamage(attack);
+        }
+    }
+    
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+    }
+
+
+ // Cosas relacionadas a la experiencia
+    public void AddExperience(int amount)
+    {
+        currentExperience += amount;
+        Debug.Log("VALOR DE EXP AL ENTRAR A ADDEXPERIENCE: " + amount);
+        if(currentExperience >= maxExperience)
+        {
+            LevelUp();
+        }
+    }
+
+    private void LevelUp()
+    {
+        maxHealth += 10;
+        attack += 10;
+        currentHealth = maxHealth;
+        currentLevel++;
+        currentExperience = 0;
+
+        maxExperience += 100;
+    }
+}
